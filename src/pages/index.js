e5336b3f157d9918f4040dd1f4f46d80682ef194
Ashley Vick <ashleyvick@github.com>
2021-10-07 19:46:06 -0600

    added metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Ashley Vick - Portfolio'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Ashley Vick - Portfolio'} />
       </Helmet>
       <App />
     </>
